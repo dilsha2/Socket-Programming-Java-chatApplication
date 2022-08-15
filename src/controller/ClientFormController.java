95d09730290ac7061244f2f5b13aab77e6abb604
dilsha2 <diliprasanna96@gmail.com>
2022-08-15 10:20:44 +0530

    Update clientForm

--- src/controller/ClientFormController.java
+++ src/controller/ClientFormController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class ClientFormController {
+    public Label lblClientName;
+    public VBox vBox;
+    public TextField txtMsg;
+
+    public void msgSendOnAction(ActionEvent actionEvent) {
+    }
 }
