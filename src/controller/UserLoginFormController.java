9838964603d77bd43bbe690651c391851c498d1e
dilsha2 <diliprasanna96@gmail.com>
2022-08-15 10:08:58 +0530

    Create iserloginform controllers content

--- src/controller/UserLoginFormController.java
+++ src/controller/UserLoginFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class UserLoginFormController {
+    public AnchorPane loginFormContext;
+    public TextField txtUserName;
+    static String userName;
+
+    public void LogInAction(ActionEvent actionEvent) throws IOException {
+        userName=txtUserName.getText();
+        Stage stage = (Stage) txtUserName.getScene().getWindow();
+        stage.close();
+        Stage stage1=new Stage();
+        stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ClientForm.fxml"))));
+        stage1.setResizable(false);
+        //primaryStage.getIcons().add(new Image("location"));
+        stage1.setTitle("Group Chat Application");
+        stage1.centerOnScreen();
+        stage1.show();
+    }
 }
